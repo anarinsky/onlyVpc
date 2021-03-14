149a82e14cf362028de57d4144b050caa61515be
anarinsky <anarinsky@192.168.7.221>
2021-03-14 10:24:01 -0400

    removing not needed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,11 +11,3 @@ variable "public_subnets" {
   default = []
   type    = list
 }
-variable "database_subnets" {
-  default = []
-  type    = list
-}
-
-variable "database_username" {}
-variable "database_password" {}
-variable "database_name" {}
